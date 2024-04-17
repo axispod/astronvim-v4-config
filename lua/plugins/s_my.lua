1fcef97cff7070390df0e2815782bcf82344cfd8
Dmitry Danilin <axispod@gmail.com>
2024-04-17 13:06:49 +0500

    Changed base configuration

--- lua/plugins/s_my.lua
+++ lua/plugins/s_my.lua
@@ -109,18 +109,12 @@ return {
         -- define the separators between each section
         separators = {
           left = { "", "" }, -- separator for the left side of the statusline
-          right = { " ", "" }, -- separator for the right side of the statusline
+          right = { " ", "" }, -- separator for the right side of the statusline
           tab = { "", "" },
         },
         -- add new colors that can be used by heirline
         colors = function(hl)
           local get_hlgroup = require("astroui").get_hlgroup
-          -- use helper function to get highlight group properties
-          local comment_fg = get_hlgroup("Comment").fg
-          hl.git_branch_fg = comment_fg
-          hl.git_added = comment_fg
-          hl.git_changed = comment_fg
-          hl.git_removed = comment_fg
           hl.blank_bg = get_hlgroup("Folded").fg
           hl.file_info_bg = get_hlgroup("Visual").bg
           hl.nav_icon_bg = get_hlgroup("String").fg
@@ -143,15 +137,10 @@ return {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
       local status = require("astroui.status")
-      opts.statusline = {
-        -- default highlight for the entire statusline
-        hl = { fg = "fg", bg = "bg" },
-        -- each element following is a component in astroui.status module
 
-        -- add the vim mode component
-        ViMode,
-        ViModeSeparator,
-        -- add a section for the currently opened file information
+      opts.statusline = { -- statusline
+        hl = { fg = "fg", bg = "bg" },
+        { ViMode, ViModeSeparator },
         status.component.file_info({
           -- enable the file_icon and disable the highlighting based on filetype
           filename = { fallback = "Empty" },
@@ -163,101 +152,116 @@ return {
           -- define the section separator
           surround = { separator = "left", condition = false },
         }),
-        -- add a component for the current git branch if it exists and use no separator for the sections
         status.component.git_branch({
-          git_branch = { padding = { left = 1 } },
-          surround = { separator = "none" },
+          git_branch = { padding = { left = 1 } }
         }),
-        -- add a component for the current git diff if it exists and use no separator for the sections
-        status.component.git_diff({
-          padding = { left = 1 },
-          surround = { separator = "none" },
-        }),
-        -- fill the rest of the statusline
-        -- the elements after this will appear in the middle of the statusline
+        status.component.git_diff(),
+        status.component.diagnostics(),
         status.component.fill(),
-        -- add a component to display if the LSP is loading, disable showing running client names, and use no separator
-        status.component.lsp({
-          lsp_client_names = false,
-          surround = { separator = "none", color = "bg" },
-        }),
-        -- fill the rest of the statusline
-        -- the elements after this will appear on the right of the statusline
+        status.component.cmd_info(),
         status.component.fill(),
-        -- add a component for the current diagnostics if it exists and use the right separator for the section
-        status.component.diagnostics({ surround = { separator = "right" } }),
-        -- add a component to display LSP clients, disable showing LSP progress, and use the right separator
-        status.component.lsp({
-          lsp_progress = false,
-          surround = { separator = "right" },
+        status.component.lsp(),
+        status.component.virtual_env(),
+        --status.component.treesitter(),
+        status.component.nav({
+          scrollbar = false,
+          hl = { fg = 'bg' },
+          ruler = { padding = { left = 1 } },
+          surround = { color = { main = 'nav_fg' }}
         }),
-        -- NvChad has some nice icons to go along with information, so we can create a parent component to do this
-        -- all of the children of this table will be treated together as a single component
-        {
-          -- define a simple component where the provider is just a folder icon
-          status.component.builder({
-            -- astronvim.get_icon gets the user interface icon for a closed folder with a space after it
-            { provider = require("astroui").get_icon("FolderClosed") },
-            -- add padding after icon
-            padding = { right = 1 },
-            -- set the foreground color to be used for the icon
-            hl = { fg = "bg" },
-            -- use the right separator and define the background color
-            surround = { separator = "right", color = "folder_icon_bg" },
-          }),
-          -- add a file information component and only show the current working directory name
-          status.component.file_info({
-            -- we only want filename to be used and we can change the fname
-            -- function to get the current working directory name
-            filename = {
-              fname = function(nr)
-                return vim.fn.getcwd(nr)
-              end,
-              padding = { left = 1 },
-            },
-            -- disable all other elements of the file_info component
-            filetype = false,
-            file_icon = false,
-            file_modified = false,
-            file_read_only = false,
-            -- use no separator for this part but define a background color
-            surround = {
-              separator = "none",
-              color = "file_info_bg",
-              condition = false,
-            },
-          }),
-        },
-        -- the final component of the NvChad statusline is the navigation section
-        -- this is very similar to the previous current working directory section with the icon
-        { -- make nav section with icon border
-          -- define a custom component with just a file icon
-          status.component.builder({
-            { provider = require("astroui").get_icon("ScrollText") },
-            -- add padding after icon
-            padding = { right = 1 },
-            -- set the icon foreground
-            hl = { fg = "bg" },
-            -- use the right separator and define the background color
-            -- as well as the color to the left of the separator
-            surround = {
-              separator = "right",
-              color = { main = "nav_icon_bg", left = "file_info_bg" },
-            },
-          }),
-          -- add a navigation component and just display the percentage of progress in the file
-          status.component.nav({
-            -- add some padding for the percentage provider
-            percentage = { padding = { right = 1 } },
-            -- disable all other providers
-            ruler = false,
-            scrollbar = false,
-            -- use no separator and define the background color
-            surround = { separator = "none", color = "file_info_bg" },
-          }),
-        },
+        status.component.mode({
+          surround = {
+            separator = "right",
+            color = { main = status.hl.mode_bg(), left = 'nav_fg' },
+            update = {  "ModeChanged", pattern = "*:*" } },
+        }),
+      }
+
+      -- opts.winbar = { -- winbar
+      --   init = function(self)
+      --     self.bufnr = vim.api.nvim_get_current_buf()
+      --   end,
+      --   fallthrough = false,
+      --   { -- inactive winbar
+      --     condition = function()
+      --       return not status.condition.is_active()
+      --     end,
+      --     status.component.separated_path(),
+      --     status.component.file_info({
+      --       file_icon = {
+      --         hl = status.hl.file_icon("winbar"),
+      --         padding = { left = 0 },
+      --       },
+      --       filename = {},
+      --       filetype = false,
+      --       file_read_only = false,
+      --       hl = status.hl.get_attributes("winbarnc", true),
+      --       surround = false,
+      --       update = "BufEnter",
+      --     }),
+      --   },
+      --   { -- active winbar
+      --     status.component.breadcrumbs({
+      --       hl = status.hl.get_attributes("winbar", true),
+      --     }),
+      --   },
+      -- }
+      --
+      -- opts.tabline = { -- tabline
+      --   { -- file tree padding
+      --     condition = function(self)
+      --       self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
+      --       self.winwidth = vim.api.nvim_win_get_width(self.winid)
+      --       return self.winwidth ~= vim.o.columns -- only apply to sidebars
+      --         and not require("astrocore.buffer").is_valid(
+      --           vim.api.nvim_win_get_buf(self.winid)
+      --         ) -- if buffer is not in tabline
+      --     end,
+      --     provider = function(self)
+      --       return (" "):rep(self.winwidth + 1)
+      --     end,
+      --     hl = { bg = "tabline_bg" },
+      --   },
+      --   status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
+      --   status.component.fill({ hl = { bg = "tabline_bg" } }), -- fill the rest of the tabline with background color
+      --   { -- tab list
+      --     condition = function()
+      --       return #vim.api.nvim_list_tabpages() >= 2
+      --     end, -- only show tabs if there are more than one
+      --     status.heirline.make_tablist({ -- component for each tab
+      --       provider = status.provider.tabnr(),
+      --       hl = function(self)
+      --         return status.hl.get_attributes(
+      --           status.heirline.tab_type(self, "tab"),
+      --           true
+      --         )
+      --       end,
+      --     }),
+      --     { -- close button for current tab
+      --       provider = status.provider.close_button({
+      --         kind = "TabClose",
+      --         padding = { left = 1, right = 1 },
+      --       }),
+      --       hl = status.hl.get_attributes("tab_close", true),
+      --       on_click = {
+      --         callback = function()
+      --           require("astrocore.buffer").close_tab()
+      --         end,
+      --         name = "heirline_tabline_close_tab_callback",
+      --       },
+      --     },
+      --   },
+      -- }
+
+      opts.statuscolumn = { -- statuscolumn
+        init = function(self)
+          self.bufnr = vim.api.nvim_get_current_buf()
+        end,
+        status.component.foldcolumn(),
+        status.component.numbercolumn(),
+        status.component.signcolumn(),
       }
     end,
-  },
+  }
 }
 
